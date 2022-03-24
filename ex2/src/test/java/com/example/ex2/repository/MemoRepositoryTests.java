c6e6d447ba147a2a4d8412596a1b674570337da6
Jeon-man <gusals236@gmail.com>
2022-03-24 18:50:27 +0900

    3월 24일

---
+++ ex2/src/test/java/com/example/ex2/repository/MemoRepositoryTests.java
@@ -0,0 +1,58 @@
+package com.example.ex2.repository;
+
+import com.example.ex2.entity.Memo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemoRepositoryTests {
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    @Test
+    public void testClass() {
+        System.out.println(memoRepository.getClass().getName());
+    }
+
+    @Test
+    public void tstInsertDummies() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().memoText("Sample..." + i).build();
+            memoRepository.save(memo);
+        });
+    }
+
+
+    @Test
+    public void testSelect() {
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById((mno));
+
+        System.out.println("============================");
+
+        if(result.isPresent()) {
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSelect2() {
+
+        Long mno = 100L;
+        Memo memo = memoRepository.getOne(mno);
+
+        System.out.println("==============================");
+
+        System.out.println(memo);
+    }
+
+}
+
+
